6993e98f38191f2b407edc7a79e66e6d59a47756
Aleksandr Borisenko <alexcustos@gmail.com>
2016-10-28 11:10:27 +0300

    New Android Studio project

---
+++ app/src/main/java/com/bytesforge/linkasanote/LinksActivity.java
@@ -0,0 +1,13 @@
+package com.bytesforge.linkasanote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.links_activity);
+    }
+}
